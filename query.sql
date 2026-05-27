fc22608fe562e38f394d7663b927fac0a1b0e5a0
Kashvi Sharma <kashvi.sharma5944@gmail.com>
2026-05-27 13:40:12 +0530

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,5 +1,6 @@
-create database Ola;
-use Ola;
+-- drop database Ola;
+-- create database Ola;
+-- use Ola;
 -- 1. Retrieve all successful bookings:
 
 create view successful_bookings as
@@ -35,7 +36,26 @@ Select count(*) from Bookings where Canceled_Rides_by_Driver != 'Personal & Car'
 Select * from cancelled_rides_drivers;
 
 -- 6. Find the maximum and minimum driver ratings for Prime Sedan bookings:
+
+create view  Max_Min_Driver_Rating as
+Select max(Driver_Ratings) as max_rate, min(Driver_Ratings) as min_rate from Bookings where Vehicle_Type = "Prime Sedan" ;
+
 -- 7. Retrieve all rides where payment was made using UPI:
+
+create view Payment_method_UPI as
+select * from Bookings where Payment_method = "UPI";
+
 -- 8. Find the average customer rating per vehicle type:
+
+create view Rating_per_vehicle as 
+select Vehicle_type , avg(Customer_Rating) from Bookings group by Vehicle_type;
+
 -- 9. Calculate the total booking value of rides completed successfully:
+
+create view total_successful_value as
+select sum(Booking_value) as total_successful_value from bookings where Booking_Status = "Success";
+
 -- 10. List all incomplete rides along with the reason:
+
+create view incomplete_rides as
+select Booking_id , Incomplete_Rides_reason from Bookings where Incomplete_Rides = "Yes";
